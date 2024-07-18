616d42311d2fe5b443c930f37020fa169d064737
amirhilal044 <amiramirhilal@gmail.com>
2024-07-18 11:53:15 +0300

    added my bookings button

--- scripts/common/landing.js
+++ scripts/common/landing.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const datepickerBtn = document.getElementById("date");
-  const passengerBtn = document.getElementById("passengers");
-  const datepicker = document.getElementById("date-input");
-  const passengerSelect = document.getElementById("select-passengers");
-  const closeMenuBtn = document.querySelector(".btn-close-menu");
-  const openMenuBtn = document.querySelector(".btn-open-menu");
-  const menu = document.querySelector(".menu");
-  const buySearchBtns = document.querySelectorAll(".btn-buy-search");
-  const navbarLinks = document.querySelector(".navbar-links");
+  const datepickerBtn = document.getElementById('date');
+  const passengerBtn = document.getElementById('passengers');
+  const datepicker = document.getElementById('date-input');
+  const passengerSelect = document.getElementById('select-passengers');
+  const closeMenuBtn = document.querySelector('.btn-close-menu');
+  const openMenuBtn = document.querySelector('.btn-open-menu');
+  const menu = document.querySelector('.menu');
+  const buySearchBtns = document.querySelectorAll('.btn-buy-search');
+  const navbarLinks = document.querySelector('.navbar-links');
   const btnSearch = document.getElementById('btn-search');
   const flightsSection = document.getElementById('tickets-section');
   const flightsContainer = document.getElementById('flights-container');
@@ -22,16 +22,18 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function updateNavbar() {
-    const token = getCookie("token");
+    const token = getCookie('token');
     if (token) {
       // User is logged in
       navbarLinks.innerHTML = `
         <a href="/index.html" class="active-link">Home</a>
         <a href="/pages/common/chatbot.html">Flightsteam AI</a>
+        <a href = "/pages/user/myBookings.html">My Bookings</a>
         <a href="#" id="logout">Log out</a>
       `;
-      document.getElementById("logout").addEventListener("click", () => {
-        document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+      document.getElementById('logout').addEventListener('click', () => {
+        document.cookie =
+          'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
         window.location.href = '/index.html';
       });
     } else {
@@ -50,62 +52,74 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const picker = new Pikaday({
     field: datepicker,
-    format: "DD/MM/YYYY",
+    format: 'DD/MM/YYYY',
     minDate: new Date(),
     onSelect: function (date) {
       console.log(date);
     },
   });
 
-  datepickerBtn.addEventListener("click", () => {
+  datepickerBtn.addEventListener('click', () => {
     picker.show();
   });
 
-  openMenuBtn.addEventListener("click", () => {
-    menu.classList.remove("closed");
-    closeMenuBtn.classList.remove("closed");
+  openMenuBtn.addEventListener('click', () => {
+    menu.classList.remove('closed');
+    closeMenuBtn.classList.remove('closed');
   });
-  closeMenuBtn.addEventListener("click", () => {
-    menu.classList.add("closed");
-    closeMenuBtn.classList.add("closed");
+  closeMenuBtn.addEventListener('click', () => {
+    menu.classList.add('closed');
+    closeMenuBtn.classList.add('closed');
   });
 
   buySearchBtns.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      if (!btn.classList.contains("active")) {
-        const shownSection = document.querySelectorAll(".show");
-        const hiddenSection = document.querySelectorAll(".hide");
+    btn.addEventListener('click', () => {
+      if (!btn.classList.contains('active')) {
+        const shownSection = document.querySelectorAll('.show');
+        const hiddenSection = document.querySelectorAll('.hide');
 
         Array.from(buySearchBtns)
-          .find((btn) => btn.classList.contains("active"))
-          .classList.remove("active");
-        btn.classList.add("active");
+          .find((btn) => btn.classList.contains('active'))
+          .classList.remove('active');
+        btn.classList.add('active');
 
-        shownSection.forEach((sec) => sec.classList.remove("show"));
-        shownSection.forEach((sec) => sec.classList.add("hide"));
-        hiddenSection.forEach((sec) => sec.classList.add("show"));
-        hiddenSection.forEach((sec) => sec.classList.remove("hide"));
+        shownSection.forEach((sec) => sec.classList.remove('show'));
+        shownSection.forEach((sec) => sec.classList.add('hide'));
+        hiddenSection.forEach((sec) => sec.classList.add('show'));
+        hiddenSection.forEach((sec) => sec.classList.remove('hide'));
       }
     });
   });
 
   // Function to check if search button should be enabled
   function checkSearchButton() {
-    const departureCity = document.getElementById('departure-city-input').value.trim();
-    const arrivalCity = document.getElementById('arrival-city-input').value.trim();
-    const passengers = document.getElementById('select-passengers').value.trim();
+    const departureCity = document
+      .getElementById('departure-city-input')
+      .value.trim();
+    const arrivalCity = document
+      .getElementById('arrival-city-input')
+      .value.trim();
+    const passengers = document
+      .getElementById('select-passengers')
+      .value.trim();
     btnSearch.disabled = !(departureCity || arrivalCity || passengers);
-    btnSearch.disabled ? fetchFlights() : console.log("no data");
+    btnSearch.disabled ? fetchFlights() : console.log('no data');
   }
 
-  document.getElementById('departure-city-input').addEventListener('input', checkSearchButton);
-  document.getElementById('arrival-city-input').addEventListener('input', checkSearchButton);
-  document.getElementById('select-passengers').addEventListener('input', checkSearchButton);
+  document
+    .getElementById('departure-city-input')
+    .addEventListener('input', checkSearchButton);
+  document
+    .getElementById('arrival-city-input')
+    .addEventListener('input', checkSearchButton);
+  document
+    .getElementById('select-passengers')
+    .addEventListener('input', checkSearchButton);
 
   function fetchFlights() {
     fetch('http://localhost/flightsteam-back-end/api/flights/getAll.php')
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         if (data.status === 200) {
           flightsData = data.data.flights;
           renderFlights(flightsData);
@@ -114,9 +128,10 @@ document.addEventListener('DOMContentLoaded', () => {
           flightsContainer.innerHTML = '<p>No flights found.</p>';
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error('Error fetching flights:', error);
-        flightsContainer.innerHTML = '<p>Error loading flights. Please try again later.</p>';
+        flightsContainer.innerHTML =
+          '<p>Error loading flights. Please try again later.</p>';
       });
     populateCityDatalists(flightsData); // Populate datalists with cities
   }
@@ -125,7 +140,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const departureCities = new Set();
     const arrivalCities = new Set();
 
-    flights.forEach(flight => {
+    flights.forEach((flight) => {
       departureCities.add(flight.departure_city);
       arrivalCities.add(flight.arrival_city);
     });
@@ -136,24 +151,23 @@ document.addEventListener('DOMContentLoaded', () => {
     departureDatalist.innerHTML = '';
     arrivalDatalist.innerHTML = '';
 
-    departureCities.forEach(city => {
+    departureCities.forEach((city) => {
       const option = document.createElement('option');
       option.value = city;
       departureDatalist.appendChild(option);
     });
 
-    arrivalCities.forEach(city => {
+    arrivalCities.forEach((city) => {
       const option = document.createElement('option');
       option.value = city;
       arrivalDatalist.appendChild(option);
     });
-
   }
 
   function renderFlights(flights) {
     flightsContainer.innerHTML = '';
 
-    flights.forEach(flight => {
+    flights.forEach((flight) => {
       const flightElement = document.createElement('div');
       flightElement.classList.add('ticket');
       flightElement.innerHTML = `
@@ -187,7 +201,10 @@ document.addEventListener('DOMContentLoaded', () => {
         <span>${new Date(flight.arrival_time).toLocaleTimeString()}</span>
         <span>$${flight.price}</span>
         <p>${flight.departure_city_code}</p>
-        <p class="duration">${Math.round((new Date(flight.arrival_time) - new Date(flight.departure_time)) / (1000 * 60 * 60))} hours</p>
+        <p class="duration">${Math.round(
+          (new Date(flight.arrival_time) - new Date(flight.departure_time)) /
+            (1000 * 60 * 60)
+        )} hours</p>
         <p>${flight.arrival_city_code}</p>
         <button class="btn-info">More info</button>
         <button class="btn-book">Book this flight</button>
@@ -196,7 +213,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
       // Add event listener to the "Book this flight" button
       flightElement.querySelector('.btn-book').addEventListener('click', () => {
-        const token = getCookie("token");
+        const token = getCookie('token');
         if (token) {
           // User is logged in, redirect to bookflight page
           window.location.href = `/pages/user/bookflight.html?flight_id=${flight.flight_id}`;
@@ -217,44 +234,66 @@ document.addEventListener('DOMContentLoaded', () => {
       </li>
     `;
 
-    flights.forEach(flight => {
+    flights.forEach((flight) => {
       const flightElement = document.createElement('li');
       flightElement.innerHTML = `
         <p>${flight.flight_number}</p>
         <p>${flight.status || 'Scheduled'}</p>
         <p>${flight.departure_city} - ${flight.arrival_city}</p>
-        <p>${new Date(flight.departure_time).toLocaleString()} - ${new Date(flight.arrival_time).toLocaleString()}</p>
+        <p>${new Date(flight.departure_time).toLocaleString()} - ${new Date(
+        flight.arrival_time
+      ).toLocaleString()}</p>
       `;
       flightsList.appendChild(flightElement);
     });
   }
 
   function filterFlightsByNumber() {
-    const searchInput = document.getElementById('search-input').value.trim().toLowerCase();
-    const filteredFlights = flightsData.filter(flight => flight.flight_number.toLowerCase().includes(searchInput));
+    const searchInput = document
+      .getElementById('search-input')
+      .value.trim()
+      .toLowerCase();
+    const filteredFlights = flightsData.filter((flight) =>
+      flight.flight_number.toLowerCase().includes(searchInput)
+    );
     renderFlightsList(filteredFlights);
   }
 
-  document.querySelector('.btn-search-flight').addEventListener('click', filterFlightsByNumber);
-  document.getElementById('search-input').addEventListener('input', filterFlightsByNumber);
+  document
+    .querySelector('.btn-search-flight')
+    .addEventListener('click', filterFlightsByNumber);
+  document
+    .getElementById('search-input')
+    .addEventListener('input', filterFlightsByNumber);
 
   function filterFlights() {
-    const departureCity = document.getElementById('departure-city-input').value.trim().toLowerCase();
-    const arrivalCity = document.getElementById('arrival-city-input').value.trim().toLowerCase();
-    const passengers = document.getElementById('select-passengers').value.trim();
+    const departureCity = document
+      .getElementById('departure-city-input')
+      .value.trim()
+      .toLowerCase();
+    const arrivalCity = document
+      .getElementById('arrival-city-input')
+      .value.trim()
+      .toLowerCase();
+    const passengers = document
+      .getElementById('select-passengers')
+      .value.trim();
 
-    const filteredFlights = flightsData.filter(flight => {
-        // Convert flight data to lowercase for case-insensitive comparison
-        const flightDepartureCity = flight.departure_city.toLowerCase();
-        const flightArrivalCity = flight.arrival_city.toLowerCase();
+    const filteredFlights = flightsData.filter((flight) => {
+      // Convert flight data to lowercase for case-insensitive comparison
+      const flightDepartureCity = flight.departure_city.toLowerCase();
+      const flightArrivalCity = flight.arrival_city.toLowerCase();
 
-        // Check conditions only if respective parameters are provided
-        const matchesDepartureCity = !departureCity || flightDepartureCity.includes(departureCity);
-        const matchesArrivalCity = !arrivalCity || flightArrivalCity.includes(arrivalCity);
-        const matchesPassengers = !passengers || flight.available_seats >= parseInt(passengers);
+      // Check conditions only if respective parameters are provided
+      const matchesDepartureCity =
+        !departureCity || flightDepartureCity.includes(departureCity);
+      const matchesArrivalCity =
+        !arrivalCity || flightArrivalCity.includes(arrivalCity);
+      const matchesPassengers =
+        !passengers || flight.available_seats >= parseInt(passengers);
 
-        // Include the flight if all provided conditions are met
-        return matchesDepartureCity && matchesArrivalCity && matchesPassengers;
+      // Include the flight if all provided conditions are met
+      return matchesDepartureCity && matchesArrivalCity && matchesPassengers;
     });
 
     renderFlights(filteredFlights);
